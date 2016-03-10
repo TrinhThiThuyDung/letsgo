77489abf5a39d03a06a7505740a71b6917086406
Thuy Dung <thuydung.kid@gmail.com>
2016-03-10 11:10:36 +0700

    design flow page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,31 @@
+<<!DOCTYPE html>
+<html>
+<head>
+	<title> Let's Go | Nha Anh </title>
+
+	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- ===================== CSS ==================== -->
+    <link rel="stylesheet" href="{{url('css/grid.css')}}">
+    <link rel="stylesheet" href="{{url('fonts/latin.css')}}">
+
+   <link rel="stylesheet" href="{{url('css/style.css')}}">
+
+    <link rel="stylesheet" href="{{url('css/bootstrap.css')}}">
+    <link rel="stylesheet" href="{{url('css/bootstrap.min.css')}}">
+
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
+  	<!-- ========================= JS ======================== -->
+    <script src="{{url('js/jquery-1.12.0.min.js')}}"></script>
+    <script src="{{url('js/jquery-2.2.0.min.js')}}"></script>
+    <script src="{{url('js/bootstrap.min.js')}}"></script>
+    <script src="{{url('js/main.js')}}"></script>
+</head>
+<body id="myPage" data-spy="scroll" data-target=".navbar" data-offset="50">
+<nav class="navbar navbar-default navbar-fixed-top">
+	<div class="container-fluid">
+		<div class=""></div>
+	</div>
+</nav>
+</body>
+</html>
